aa7da95c606fb5a8e61b48534cb4a09b58e9cc84
Maria Eduarda Santos <mariaeduardasanntoss@gmail.com>
2022-04-18 20:20:35 -0300

    queries users

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,9 +1,12 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository} from 'typeorm';
 
 import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
 import { User } from '../../entities/User';
 import { IUsersRepository } from '../IUsersRepository';
 
+interface IRequest {
+  user: string;
+}
 export class UsersRepository implements IUsersRepository {
   private repository: Repository<User>;
 
@@ -11,20 +14,31 @@ export class UsersRepository implements IUsersRepository {
     this.repository = getRepository(User);
   }
 
-  async findUserWithGamesById({
-    user_id,
-  }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+  async findUserWithGamesById({user_id}: IFindUserWithGamesDTO): Promise<User> {
+    const user = await this.repository.findOne(user_id, { relations: ['games'] });
+
+    if (!user) {
+      throw new Error("User not exists")
   }
+  return user;
+}
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
-  }
+    // Complete usando raw query
+    const queryAllUsers = 
+    ` SELECT  id, first_name, last_name, email, created_at, updated_at 
+    FROM users 
+    ORDER BY first_name `
+  ;
+  return this.repository.query(queryAllUsers);
+}
 
-  async findUserByFullName({
-    first_name,
-    last_name,
-  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+  async findUserByFullName({first_name,last_name,}: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+     // Complete usando raw query
+    const query = ` SELECT email, first_name, last_name
+    FROM users
+    WHERE LOWER(first_name) = LOWER($1)
+    AND LOWER(last_name) = LOWER($2) `;
+  return this.repository.query(query, [first_name, last_name]);
   }
 }
